00afc3d6d92ba4ee2615b6160c6ba8d8ba53fafc
Hygrafik <x.alexlalvay@gmail.com>
2022-11-02 02:39:36 -0500

    Initial commirert

---
+++ view/foother.php
@@ -0,0 +1,14 @@
+<footer class="text-muted py-4 bg-nav footer">
+    <div class="container">
+        <p class="float-end mb-1 ">
+
+            <a href="#" class="text-white-50 text-decoration-none">SUBIR</a>
+        </p>
+        <p class="mb-1">POWER BY &copy; HYGRAFIK</p>
+        <p> <?php
+            $Year = date("Y");
+            echo "$Year.";
+            echo "\n";
+            ?>  </p>
+    </div>
+</footer>
---
+++ view/header.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4 class="text-white">Museo</h4>
+                    <p class="text-white-50">Expone 245 piezas declaradas patrimoniales culturales, con relatos de 4.500 años de las culturas Valdivia, Machalilla, chorrera-Engoroy, Bahía, Guangala y Manteña.</p>
+                </div>
+                <div class="col-sm-4 offset-md-1 py-4">
+                    <h4 class="text-white">Contact</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.google.com/maps/dir/-1.5906262,-80.842832/Museo+Arqueologico+De+Salango+los+balceros+del+sur+y+calle4+Salango+130315/@-1.5906798,-80.8429607,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x902e9dc58b47614b:0x309300df1e1c20c0!2m2!1d-80.842831!2d-1.5906111" class="text-warning">Ubicación</a></li>
+                        <li><a href="https://comunidadsalango.com/" class="text-warning">Sitio Web</a></li>
+                        <li><a href="https://api.whatsapp.com/send?phone=593985543879" class="text-warning">Whatsapp</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar navbar-dark bg-brown shadow-sm">
+        <div class="container">
+            <a href="../" class="navbar-brand d-flex align-items-center">
+                <i class="fi fi-rr-angle-left text-white-50 "></i>&nbsp;<strong class="text-uppercase text-white-50 ">volver</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+</header>
